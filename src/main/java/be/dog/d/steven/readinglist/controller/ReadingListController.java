8e524a19309209ea411a8ab3a9c5569759c36d07
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-11-22 19:16:11 +0100

    :recycle: Changes to controller

--- src/main/java/be/dog/d/steven/readinglist/controller/ReadingListController.java
+++ src/main/java/be/dog/d/steven/readinglist/controller/ReadingListController.java
@@ -2,11 +2,12 @@ package be.dog.d.steven.readinglist.controller;
 
 import be.dog.d.steven.readinglist.dao.ReadingListDao;
 import be.dog.d.steven.readinglist.model.Book;
+import be.dog.d.steven.readinglist.security.Reader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,8 +15,10 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/")
+@ConfigurationProperties(prefix="amazon")
 public class ReadingListController {
 
+    private String associateId;
     private final ReadingListDao readingListDao;
 
     @Autowired
@@ -23,21 +26,26 @@ public class ReadingListController {
         this.readingListDao = readingListDao;
     }
 
-    @GetMapping(value = "/{reader}")
-    public String readersBooks(@PathVariable("reader") String reader, Model model){
-        List<Book> readingList = readingListDao.findByReader(reader);
-        if(readingList != null){
+    public void setAssociateId(String associateId) {
+        this.associateId = associateId;
+    }
+
+    @GetMapping
+    public String readersBooks(Reader reader, Model model) {
+        List<Book> readingList = readingListDao.findByReader(reader.getUsername());
+        if (readingList != null) {
             model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
+            model.addAttribute("amazonID", associateId);
         }
         return "readingList";
     }
 
-    @PostMapping(value = "/{reader}")
-    public String addToReadingList(@PathVariable("reader") String reader, Book book){
-        book.setReader(reader);
+    @PostMapping
+    public String addToReadingList(Reader reader, Book book) {
+        book.setReader(reader.getUsername());
         readingListDao.save(book);
-
-        return "redirect:/{reader}";
+        return "redirect:/";
     }
 
 }
