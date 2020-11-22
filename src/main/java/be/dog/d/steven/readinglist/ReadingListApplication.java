a032aa1c6eb1b01c276c9ee72383fe6a654fe092
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-11-22 19:19:22 +0100

    :building_construction: Reader and login handling

--- src/main/java/be/dog/d/steven/readinglist/ReadingListApplication.java
+++ src/main/java/be/dog/d/steven/readinglist/ReadingListApplication.java
@@ -1,13 +1,29 @@
 package be.dog.d.steven.readinglist;
 
+import be.dog.d.steven.readinglist.security.ReaderHandlerMethodArgumentResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @SpringBootApplication
-public class ReadingListApplication {
+public class ReadingListApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(ReadingListApplication.class, args);
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addArgumentResolvers(
+            List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new ReaderHandlerMethodArgumentResolver());
+    }
 }
