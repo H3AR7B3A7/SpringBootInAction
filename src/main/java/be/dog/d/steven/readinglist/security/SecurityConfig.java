9aa5cf66f845bf7b19a4b74b09f39c4c8e6775ad
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-11-22 14:49:00 +0100

    :bug: Fix

--- src/main/java/be/dog/d/steven/readinglist/security/SecurityConfig.java
+++ src/main/java/be/dog/d/steven/readinglist/security/SecurityConfig.java
@@ -1,14 +1,20 @@
 package be.dog.d.steven.readinglist.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     private ReaderDao readerDao;
 
     @Override
@@ -21,8 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
 
                 .formLogin()
-                    .loginPage("/login")
-                    .failureUrl("/login?error=true");
+                .loginPage("/login")
+                .failureUrl("/login?error=true");
     }
 
     @Override
