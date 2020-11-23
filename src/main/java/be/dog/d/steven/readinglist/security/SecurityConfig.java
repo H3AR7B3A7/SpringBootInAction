4d0b3623f4cb9414b9850ef7601e39fddd28df4e
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-11-23 01:11:53 +0100

    :construction: CSRF temporarily disabled

--- src/main/java/be/dog/d/steven/readinglist/security/SecurityConfig.java
+++ src/main/java/be/dog/d/steven/readinglist/security/SecurityConfig.java
@@ -22,6 +22,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable() // Remove this and generate csrf token instead
+
                 .authorizeRequests()
                 .antMatchers("/").access("hasRole('READER')")
                 .antMatchers("/**").permitAll()
